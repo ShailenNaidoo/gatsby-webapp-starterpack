238be84d7b67533fc939731767d6ce9591b1bf73
Schalk Venter <venterschalk@gmail.com>
2018-12-18 19:58:37 +0200

    Updates pre-deploy script

--- tooling/node/deployChecklist.js
+++ tooling/node/deployChecklist.js
@@ -2,33 +2,35 @@ const inquirer = require('inquirer');
 const { includes } = require('lodash');
 const { red } = require('colors');
 
-const choices = [
-  'Add/update unit tests',
-  'Add/update integration test',
-  'Add/update documentation',
-  'Add/update analytics',
-  'Check supported browsers on BrowserStack',
-];
+console.log('asdasd');
 
-inquirer
-  .prompt([
-    {
-      name: 'checklist',
-      type: 'checkbox',
-      message: 'Did you remember to do the following as required?',
-      choices,
-    },
-  ])
-  .then(({ checklist }) => {
-    // choices.forEach((item) => {
-    //   if (!includes(checklist, item)) {
-    //     throw('Deployment aborted. you did not complete all pre-deploy checks!');
-    //   }
+// const choices = [
+//   'Add/update unit tests',
+//   'Add/update integration test',
+//   'Add/update documentation',
+//   'Add/update analytics',
+//   'Check supported browsers on BrowserStack',
+// ];
 
-    //   return null;
-    // });
-  })
-  .catch((error) => {
-    // console.log(red(error));
-    // process.exit(1);
-  });
+// inquirer
+//   .prompt([
+//     {
+//       name: 'checklist',
+//       type: 'checkbox',
+//       message: 'Did you remember to do the following as required?',
+//       choices,
+//     },
+//   ])
+//   .then(({ checklist }) => {
+//     choices.forEach((item) => {
+//       if (!includes(checklist, item)) {
+//         throw('Deployment aborted. you did not complete all pre-deploy checks!');
+//       }
+
+//       return null;
+//     });
+//   })
+//   .catch((error) => {
+//     console.log(red(error));
+//     process.exit(1);
+//   });
